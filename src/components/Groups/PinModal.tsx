df9aad0bf3aafa8226c7dfc2cfa15c3bf8186163
aylivdele <irassomahin@gmail.com>
2026-02-03 06:28:05 +0500

    fix pinModal

--- src/components/Groups/PinModal.tsx
+++ src/components/Groups/PinModal.tsx
@@ -1,5 +1,7 @@
-import { Modal, PinInput } from '@telegram-apps/telegram-ui'
+import { Modal, PinInput, VisuallyHidden } from '@telegram-apps/telegram-ui'
+import { ModalHeader } from '@telegram-apps/telegram-ui/dist/components/Overlays/Modal/components/ModalHeader/ModalHeader'
 import { useEffect, useState } from 'react'
+import { Title } from './../../../node_modules/.pnpm/@telegram-apps+telegram-ui@_cd2a85882c477b95a917a92df729637e/node_modules/@telegram-apps/telegram-ui_tmp_28860/dist/components/Typography/Title/Title'
 
 export function PinModal({ open, onOpenChange, onPinEnter }: { open: boolean, onOpenChange: (isOpen: boolean) => void, onPinEnter: (pin: number[]) => void }) {
   const [value, setValue] = useState<number[]>([])
@@ -18,6 +20,11 @@ export function PinModal({ open, onOpenChange, onPinEnter }: { open: boolean, on
 
   return (
     <Modal onOpenChange={onOpenChange} open={open}>
+      <VisuallyHidden>
+        <ModalHeader>
+          Ввод пароля
+        </ModalHeader>
+      </VisuallyHidden>
       <PinInput pinCount={4} value={value} onChange={setValue} label="Введите пароль" />
     </Modal>
   )
