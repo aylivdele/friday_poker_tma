634d9b3217f1cc650ab7019d4b0787e9e848a56c
aylivdele <irassomahin@gmail.com>
2026-02-05 20:52:38 +0500

    styles

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -9,8 +9,8 @@ export function PlayerComponent({ player }: { player: Player }) {
         <Avatar size={96} src={player.avatarUrl} style={{ margin: '20px calc(50% - 48px)' }} />
         {player.telegramId
           ? (
-              <Cell before="Telegram:" onClick={() => openTelegramLink?.(`tg://user?id=${player.telegramId}`)}>
-                <Text Component="a">{player.username || player.telegramId}</Text>
+              <Cell before="Telegram:" onClick={() => openTelegramLink?.(`https://t.me/${player.username}`)}>
+                <Text Component="a">{player.username}</Text>
               </Cell>
             )
           : <Cell>Не занятый пользователь</Cell>}
