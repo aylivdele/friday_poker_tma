bbc1d52f43525c5a2de55ba0bc583e0ff43333b6
aylivdele <irassomahin@gmail.com>
2026-02-05 20:30:29 +0500

    fixes

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,16 +1,16 @@
 import type { Player } from '@/types/api'
 import { Avatar, AvatarStack, Cell, Info, List, Section, Text } from '@telegram-apps/telegram-ui'
-import { Link } from '../Link/Link'
+import { openTelegramLink } from '@tma.js/sdk-react'
 
 export function PlayerComponent({ player }: { player: Player }) {
   return (
     <Section header={`Профиль: ${player.firstName} ${player.lastName}`}>
       <List>
-        <Avatar size={96} src={player.avatarUrl} style={{ margin: '10% calc(50% - 48px)' }} />
+        <Avatar size={96} src={player.avatarUrl} style={{ margin: '20px calc(50% - 48px)' }} />
         {player.username
           ? (
-              <Cell before="Tg username:">
-                <Link href={`https://t.me/${player.username}`}>{player.username}</Link>
+              <Cell before="Tg username:" onClick={() => openTelegramLink?.(`https://t.me/${player.username}`)}>
+                <Text>{player.username}</Text>
               </Cell>
             )
           : <Cell>Не занятый пользователь</Cell>}
